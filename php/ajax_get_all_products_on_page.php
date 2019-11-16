2fa05c569b5407a51f7a1c8914059eabfc8918c1
andrz <andrz@192.168.1.8>
2019-11-16 17:15:15 +0000

    SHOP get all products php file

--- php/ajax_get_all_products_on_page.php
+++ php/ajax_get_all_products_on_page.php
@@ -2,33 +2,36 @@
 
 $default_page = 1;
 $default_pageLimit = 10; //items per page
-$page = filter_input(INPUT_POST, "pageNumber", FILTER_SANITIZE_NUMBER_INT);
-if (empty($page || $page < 0)) {
+//$page = filter_input(INPUT_POST, "pageNumber", FILTER_SANITIZE_NUMBER_INT);
+$page = 0;
+if (empty($page) || $page < 0) {
     $page = $default_page;
 }
-$pageLimit = filter_input(INPUT_POST, "pageLimit", FILTER_SANITIZE_NUMBER_INT);
-//have check if one of the allowed values e.g 10,25,50
-if (empty($pageLimit || !($pageLimit == $default_pageLimit || $pageLimit == 25 || $pageLimit == 50))) {
+//$pageLimit = filter_input(INPUT_POST, "pageLimit", FILTER_SANITIZE_NUMBER_INT);
+$pageLimit = 10;
+if (empty($pageLimit) || !($pageLimit === $default_pageLimit || $pageLimit === 25 || $pageLimit === 50)) {
     $pageLimit = $default_pageLimit;
 }
-
+//debug
+//$page = 1;
+//$pageLimit = 3;
+$pageStart = $page * $pageLimit;
 require_once "configuration.php";
-
 $dbConnection = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUsername, $dbPassword);
 $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 //pagestart is exclusive
-$query = "SELECT id, name, unit_price, image, stock FROM products LIMIT :pageStart,:pageLimit;";
+//$query = "SELECT product_id, name, unit_price, image_url, stock FROM products LIMIT :pageStart,:pageLimit";
+$query = "SELECT * FROM products LIMIT :pageStart, :pageLimit";
 $statement = $dbConnection->prepare($query);
-$statement->bindParam(":pageStart", ($page * $pageLimit), PDO::PARAM_INT);
+$statement->bindParam(":pageStart", $pageStart, PDO::PARAM_INT);
 $statement->bindParam(":pageLimit", $pageLimit, PDO::PARAM_INT);
 $statement->execute();
-
 $products = array();
 if ($statement->rowCount() > 0) {
     $result = $statement->fetchAll(PDO::FETCH_OBJ);
     foreach ($result as $row) {
         $products[] = $row;
     }
-    echo json . encode($products);
+    echo json_encode($products);
 }
