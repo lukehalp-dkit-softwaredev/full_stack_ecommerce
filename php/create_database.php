a0c90d3fda118b543e816826a485d2c1d7053843
Luke Halpenny <wildfire@pop-os.localdomain>
2019-11-17 16:14:15 +0000

    Add order tables to db

--- php/create_database.php
+++ php/create_database.php
@@ -27,23 +27,42 @@
         $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);   // set the PDO error mode to exception
 
         /* Create table */
-        $query = "CREATE TABLE products(product_id INT NOT NULL PRIMARY KEY,
-    name VARCHAR(88) NOT NULL,
-    description TEXT NOT NULL,
-    unit_price FLOAT NOT NULL,
-    image_url VARCHAR(255) NULL,
-    stock INT NOT NULL);"
-                . "CREATE TABLE users(
-    user_id INT NOT NULL PRIMARY KEY,
-    email VARCHAR(320) NOT NULL,
-    password_hash VARCHAR(255) NOT NULL,
-    billing_address VARCHAR(255) NOT NULL,
-    mc_username VARCHAR(16) NOT NULL)";
+        $query = "CREATE TABLE IF NOT EXISTS products(
+                    product_id INT NOT NULL PRIMARY KEY,
+                    name VARCHAR(88) NOT NULL,
+                    description TEXT NOT NULL,
+                    unit_price FLOAT NOT NULL,
+                    image_url VARCHAR(255) NULL,
+                    stock INT NOT NULL);"
+                . "CREATE TABLE IF NOT EXISTS users(
+                    user_id INT NOT NULL PRIMARY KEY,
+                    email VARCHAR(320) NOT NULL,
+                    password_hash VARCHAR(255) NOT NULL,
+                    billing_address VARCHAR(255) NOT NULL,
+                    mc_username VARCHAR(16) NOT NULL);"
+                . "CREATE TABLE IF NOT EXISTS orders(
+                    order_id INT NOT NULL PRIMARY KEY,
+                    user_id INT NOT NULL,
+                    date_created DATETIME DEFAULT CURRENT_TIMESTAMP,
+                    date_ordered DATETIME NULL,
+                    FOREIGN KEY (user_id) REFERENCES users(user_id));"
+                . "CREATE TABLE IF NOT EXISTS order_lines(
+                    order_id INT NOT NULL,
+                    product_id INT NOT NULL,
+                    quantity INT NOT NULL,
+                    PRIMARY KEY (order_id, product_id),
+                    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+                    FOREIGN KEY (product_id) REFERENCES products(product_id));";
         $statement = $dbConnection->prepare($query);
         $statement->execute();
 
+        $statement = $dbConnection->prepare("SHOW TABLES;");
+        $statement->execute();
+        
+        echo json_encode($statement->fetchAll(PDO::FETCH_OBJ));
+
         /* Provide feedback to the user */
-        echo "Database '$dbName' created.";
+        echo "<br>Database '$dbName' created.";
         ?>
     </body>
 </html>
