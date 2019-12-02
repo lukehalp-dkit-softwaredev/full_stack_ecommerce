532a3b6e576a07bc6b9949a5bc9b3f2d1654a801
andrz <andrz@192.168.1.7>
2019-12-02 09:23:51 +0000

    lalaa

--- api/users/logout.php
+++ api/users/logout.php
@@ -8,8 +8,8 @@ use Auth0\SDK\Auth0;
 
 $auth0 = new Auth0([
     'domain' => 'dev-44t0mog0.eu.auth0.com',
-    'client_id' => 'hzLwly8pSwfEEJPBcJXtd8HLLS6eO0ZC',
-    'client_secret' => 'oUbeVZiuepsh92ldnjHHPAuEaI2WDEjDUM7aXAN-vcONJlRZ9T5SrB-SQUwiA8Rr',
+    'client_id' => $auth0_client_id,
+    'client_secret' => $auth0_client_secret,
     'redirect_uri' => $siteName . '/index.php',
     'persist_id_token' => true,
     'persist_access_token' => true,
@@ -17,6 +17,5 @@ $auth0 = new Auth0([
         ]);
 $auth0->logout();
 $return_to = $siteName . '/index.php';
-$logout_url = sprintf('http://%s/v2/logout?client_id=%s&returnTo=%s', 'dev-44t0mog0.eu.auth0.com', 'hzLwly8pSwfEEJPBcJXtd8HLLS6eO0ZC', $return_to);
+$logout_url = sprintf('http://%s/v2/logout?client_id=%s&returnTo=%s', $auth0_domain, $auth0_client_id, $return_to);
 header('Location: ' . $logout_url);
-die();
