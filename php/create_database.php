256c3c142eac345d80c44657d0de6ea3ab4f19b3
Luke Halpenny <wildfire@pop-os.localdomain>
2019-12-02 09:25:12 +0000

    update database

--- php/create_database.php
+++ php/create_database.php
@@ -27,7 +27,10 @@
         $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);   // set the PDO error mode to exception
 
         /* Create table */
-        $query = "CREATE TABLE IF NOT EXISTS products(
+        $query = "CREATE TABLE IF NOT EXISTS categories(
+                    category_id INT NOT NULL PRIMARY KEY,
+                    name VARCHAR(64) NOT NULL);"
+                . "CREATE TABLE IF NOT EXISTS products(
                     product_id INT NOT NULL PRIMARY KEY,
                     name VARCHAR(88) NOT NULL,
                     description TEXT NOT NULL,
@@ -37,30 +40,25 @@
                     category_id INT NOT NULL,
                     FOREIGN KEY (category_id) REFERENCES categories(category_id));"
                 . "CREATE TABLE IF NOT EXISTS users(
-                    user_id INT NOT NULL PRIMARY KEY,
-                    email VARCHAR(320) NOT NULL,
-                    password_hash VARCHAR(255) NOT NULL,
-                    billing_address VARCHAR(255) NOT NULL,
-                    mc_username VARCHAR(16) NOT NULL);"
+                    user_id VARCHAR(255) NOT NULL PRIMARY KEY,
+                    mc_username VARCHAR(16) NULL,
+                    mc_uuid VARCHAR(255) NULL);"
                 . "CREATE TABLE IF NOT EXISTS orders(
-                    order_id INT NOT NULL PRIMARY KEY,
-                    user_id INT NOT NULL,
+                    order_id BIGINT NOT NULL PRIMARY KEY,
+                    user_id VARCHAR(255) NOT NULL,
                     date_created DATETIME DEFAULT CURRENT_TIMESTAMP,
                     date_ordered DATETIME NULL,
                     FOREIGN KEY (user_id) REFERENCES users(user_id));"
                 . "CREATE TABLE IF NOT EXISTS order_lines(
-                    order_id INT NOT NULL,
+                    order_id BIGINT NOT NULL,
                     product_id INT NOT NULL,
                     quantity INT NOT NULL,
                     PRIMARY KEY (order_id, product_id),
                     FOREIGN KEY (order_id) REFERENCES orders(order_id),
-                    FOREIGN KEY (product_id) REFERENCES products(product_id));"
-                . "CREATE TABLE IF NOT EXISTS categories(
-                    category_id INT NOT NULL PRIMARY KEY,
-                    name VARCHAR(64) NOT NULL);"
+                    FOREIGN KEY (product_id) REFERENCES products(product_id));";
                 /* Alter table statements for existing tables */
-                . "ALTER TABLE products ADD category_id INT NOT NULL;
-                    ALTER TABLE products ADD FOREIGN KEY (category_id) REFERENCES categories(category_id);";
+                // . "ALTER TABLE products ADD category_id INT NOT NULL;
+                //     ALTER TABLE products ADD FOREIGN KEY (category_id) REFERENCES categories(category_id);";
         $statement = $dbConnection->prepare($query);
         $statement->execute();
 
