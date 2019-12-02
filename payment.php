0b6140d2f42f35ff1610de954bf911b89da5ecdc
andrz <andrz@192.168.1.7>
2019-12-02 19:55:26 +0000

    more works on ze stripe from school time

--- payment.php
+++ payment.php
@@ -1,31 +1,91 @@
 <?php
 require_once "php/configuration.php";
+/* Connect to the database */
+$dbConnection = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUsername, $dbPassword);
+$dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);   // set the PDO error mode to exception
+
 require 'vendor/autoload.php';
 
 \Stripe\Stripe::setApiKey($stripeSK);
+\Firebase\JWT\JWT::$leeway = 60;
 
-$session = \Stripe\Checkout\Session::create([
-            'payment_method_types' => ['card'],
-            'line_items' => [
-                [
-                    'name' => 'T-shirt',
-                    'description' => 'Comfortable cotton t-shirt',
-                    'images' => ['https://example.com/t-shirt.png'],
-                    'amount' => 500,
-                    'currency' => 'eur',
-                    'quantity' => 1,
-                ], [
-                    'name' => 'T-shirt',
-                    'description' => 'Comfortable cotton t-shirt',
-                    'images' => ['https://example.com/t-shirt.png'],
-                    'amount' => 500,
-                    'currency' => 'eur',
-                    'quantity' => 1,
-                ]],
-            'success_url' => 'https://example.com/success?session_id={CHECKOUT_SESSION_ID}',
-            'cancel_url' => 'https://example.com/cancel',
+use Auth0\SDK\Auth0;
+
+$auth0 = new Auth0([
+    'domain' => $auth0_domain,
+    'client_id' => $auth0_client_id,
+    'client_secret' => $auth0_client_secret,
+    'redirect_uri' => $siteName . '/category.php',
+    'persist_id_token' => true,
+    'persist_access_token' => true,
+    'persist_refresh_token' => true,
         ]);
-echo $session;
+//get user's basket
+//get order id 
+//get lines from basket from order id
+//insert to line items
+$error = new stdClass();
+$response = new stdClass();
+$userInfo = $auth0->getUser();
+if ($userInfo) {
+    $query = "SELECT order_id FROM orders WHERE user_id = :user_id AND date_ordered IS NULL";
+    $statement = $dbConnection->prepare($query);
+    $statement->bindParam(":user_id", $userInfo["sub"], PDO::PARAM_STR);
+    $statement->execute();
+    if ($statement->rowCount() > 0) {
+        $result = $statement->fetch(PDO::FETCH_OBJ);
+        $query = "SELECT order_lines.quantity, products.product_id, products.name, products.description, products.unit_price FROM order_lines, products WHERE order_id = :order_id AND order_lines.product_id = products.product_id";
+        $statement = $dbConnection->prepare($query);
+        $statement->bindParam(":order_id", $result->order_id, PDO::PARAM_INT);
+        $statement->execute();
+        if ($statement->rowCount() > 0) {
+            $user_email = $userInfo['email'];
+            $session_object = [
+                'payment_intent_data' => [
+                    'setup_future_usage' => 'off_session',
+                ],
+//                'customer' => $userInfo["sub"],
+                'customer_email' => $user_email,
+                'payment_method_types' => ['card'],
+                'line_items' => [],
+                'success_url' => $siteName . 'confirmation.php?session_id={CHECKOUT_SESSION_ID}',
+                'cancel_url' => 'https://example.com/cancel',
+            ];
+            $result = $statement->fetchAll(PDO::FETCH_OBJ);
+            /**
+              'name' => '60-minute massage',
+              'description' => 'A 60-minute therapeutic massage',
+              'images' => ['https://example.com/massage.png'],
+              'amount' => 7000,
+              'currency' => 'eur',
+              'quantity' => 1, */
+            foreach ($result as $row) {
+                $item = [
+                    "name" => $row->name,
+                    "description" => $row->description,
+//                $session_object->line_items->images = $row["products.description"];
+                    "amount" => $row->unit_price * 100,
+                    "currency" => "eur",
+                    "quantity" => $row->quantity
+                ];
+                $session_object['line_items'][] = $item;
+            }
+            $session = \Stripe\Checkout\Session::create($session_object);
+        } else {
+            //order line empty???
+            $error->code = 404;
+            $error->msg = "Order empty";
+            $response->error = $error;
+        }
+    } else {
+        //order_id not found
+        $error->code = 404;
+        $error->msg = "Order not found";
+        $response->error = $error;
+    }
+} else {
+    header("location: api/login.php");
+}
 ?>
 
 <!DOCTYPE html>
@@ -61,6 +121,7 @@ echo $session;
 
         <script src="https://js.stripe.com/v3/"></script>
         <script>
+            console.log(<?php json_encode($session_object) ?>);
             var stripe = Stripe('<?php echo $stripePK; ?>');
             stripe.redirectToCheckout({
                 // Make the id field from the Checkout Session creation API response
@@ -76,7 +137,7 @@ echo $session;
 
     </head>
     <body>
-        
+
     </body>
 </html>
 
