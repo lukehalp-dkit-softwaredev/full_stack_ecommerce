882554ba9dc725f338bede8eb75df5a3a21fdc73
Luke Halpenny <wildfire@pop-os.localdomain>
2019-12-05 10:03:03 +0000

    fix cart css + remove shipping section

--- cart.php
+++ cart.php
@@ -174,41 +174,7 @@ $userInfo = $auth0->getUser();
                                         <h5>Subtotal</h5>
                                     </td>
                                     <td>
-                                        <h5>$2160.00</h5>
-                                    </td>
-                                </tr>
-                                <tr class="shipping_area">
-                                    <td>
-
-                                    </td>
-                                    <td>
-
-                                    </td>
-                                    <td>
-                                        <h5>Shipping</h5>
-                                    </td>
-                                    <td>
-                                        <div class="shipping_box">
-                                            <ul class="list">
-                                                <li><a href="#">Flat Rate: $5.00</a></li>
-                                                <li><a href="#">Free Shipping</a></li>
-                                                <li><a href="#">Flat Rate: $10.00</a></li>
-                                                <li class="active"><a href="#">Local Delivery: $2.00</a></li>
-                                            </ul>
-                                            <h6>Calculate Shipping <i class="fa fa-caret-down" aria-hidden="true"></i></h6>
-                                            <select class="shipping_select">
-                                                <option value="1">Bangladesh</option>
-                                                <option value="2">India</option>
-                                                <option value="4">Pakistan</option>
-                                            </select>
-                                            <select class="shipping_select">
-                                                <option value="1">Select a State</option>
-                                                <option value="2">Select a State</option>
-                                                <option value="4">Select a State</option>
-                                            </select>
-                                            <input type="text" placeholder="Postcode/Zipcode">
-                                            <a class="gray_btn" href="#">Update Details</a>
-                                        </div>
+                                        <h5 id="total_price">$0000.00</h5>
                                     </td>
                                 </tr>
                                 <tr class="out_button_area">
@@ -223,7 +189,7 @@ $userInfo = $auth0->getUser();
                                     </td>
                                     <td>
                                         <div class="checkout_btn_inner d-flex align-items-center">
-                                            <a class="gray_btn" href="#">Continue Shopping</a>
+                                            <a class="gray_btn" href="category.php">Continue Shopping</a>
                                             <a onclick="location.href = 'choose_payment_method.php'" class="primary-btn" href="#">Proceed to checkout</a>
                                         </div>
                                     </td>
